7ffcde03cfdcf6bb84a10239a5496dba030e512b
huyy802 <20521401@gm.uit.edu.vn>
2023-09-29 17:23:07 +0700

    feat: done

---
+++ BusinessObjects/MyDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessObjects
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() { }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", true, true)
+               .Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("MystoreDB"));
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder optionBuilder)
+        {
+            optionBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Beverages" },
+                new Category { CategoryId = 2, CategoryName = "Condiments" },
+                new Category { CategoryId = 3, CategoryName = "Confections" },
+                new Category { CategoryId = 4, CategoryName = "Dairy Products" },
+                new Category { CategoryId = 5, CategoryName = "Grains/Cerearls" },
+                new Category { CategoryId = 6, CategoryName = "Meat/Poultry" },
+                new Category { CategoryId = 7, CategoryName = "Produce" },
+                new Category { CategoryId = 8, CategoryName = "Seefood" }
+            );
+        }
+
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects;
+using DataAccess;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void DeleteProduct(Product p) => ProductDAO.DeleteProduct(p);
+
+        public List<Category> GetCategories() => CategoryDAO.GetCategories();
+
+        public Product GetProductById(int id) => ProductDAO.FindProductById(id);
+
+        public List<Product> GetProducts() => ProductDAO.GetProducts();
+
+        public void SaveProduct(Product p) => ProductDAO.SaveProduct(p);
+
+        public void UpdateProduct(Product p) => ProductDAO.UpdateProduct(p);
+    }
+}
